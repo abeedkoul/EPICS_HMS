57bfdd6e23061b81b042714fa0338fee9ecc9c53
abeedkoul <koulabeed@gmail.com>
2025-03-24 18:17:17 +0530

    Working on the Login Page facing some errors working on the fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import "./App.css"
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Home from "./pages/Home"
@@ -8,11 +8,26 @@ import Login from "./pages/Login"
 import AboutUs from "./pages/AboutUs"
 import { ToastContainer, toast } from 'react-toastify';
 import Navbar from './components/Navbar'
+import { Context } from './main'
+import axios from 'axios'
 
 
 export const App = () => {
 
-  
+  const {isAuthenticated, setIsAuthenticated, setUser} = useContext(Context);
+  useEffect(()=>{
+    const fetchUser = async()=>{
+      try {
+        const response = await axios.get("http://localhost:4000/api/v1/user/patient/me",{withCredentials: true});
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  },isAuthenticated)
   return (
     <>
     <Router>
