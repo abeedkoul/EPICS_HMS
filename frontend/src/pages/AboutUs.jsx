5f56c428fec0254cf927e1450d89d114126e1f4b
abeedkoul <koulabeed@gmail.com>
2025-03-24 19:20:56 +0530

    Worked and almost finished the About Us page of the Website

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Biography from "../components/Biography"
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+    <Hero title="Learn More About Us | VITCARE" imageUrl={"/about.png"}/>
+    <Biography imageUrl={"/whoweare.png"} />
+    </>
   )
 }
 
